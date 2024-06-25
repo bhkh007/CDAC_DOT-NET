9ae784fa6a6428d6ee1da70de1c2ecb45b8e63e7
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-25 19:54:24 +0530

    stated new module creating two simple projects

---
+++ ClassBasics/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ClassBasics
+{
+     class App
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("This is the first dot net program");
+        }
+    }
+}
