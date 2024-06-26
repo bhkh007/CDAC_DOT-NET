ba9153b972681d1dab4ef32190406d1fd5b52b66
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-26 20:38:07 +0530

    completing....

--- Day01/Assignment01/Program.cs
+++ Day01/Assignment01/Program.cs
@@ -7,8 +7,13 @@
             Employee emp = new Employee();
             emp.name = "Ashwin";
             emp.empNo = 101;
-            Console.WriteLine("Employee Name: " +emp.name);
-            Console.WriteLine("Employee no: "+emp.empNo);
+            emp.basic = 50000;
+            emp.deptNo = 5;
+            Console.WriteLine("Employee Name: " + emp.name);
+            Console.WriteLine("Employee no: " + emp.empNo);
+            Console.WriteLine(emp.basic);
+            Console.WriteLine("Dept no: " + emp.deptNo);
+
         }
 
         private String name;
@@ -26,16 +31,44 @@
         }
 
         private int empNo;
-        
+
         public int EmpNo
         {
             set
             {
-                if((empNo = value) != 0)
+                if ((empNo = value) != 0)
                     empNo = value;
             }
 
             get { return empNo; }
+        }
+
+        private decimal basic;
+
+        public decimal Basic
+        {
+            set
+            {
+                if (value > 0 && value < 100000)
+                    basic = value;
+                else basic = 0;
+            }
+            get { return basic; }
+        }
+
+        private short deptNo;
+
+        public short DeptNo
+        {
+            set
+            {
+                if (value >= 0)
+                    value = deptNo;
+                else value = 0;
+            }
+
+            get { return deptNo; }
+
         }
     }
 }
