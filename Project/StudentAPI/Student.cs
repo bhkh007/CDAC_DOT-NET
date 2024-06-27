643aa03fbaaf04c3d53d5fca50f839a8a18b6ef5
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-27 19:06:25 +0530

    small api project on student including get post put and delete request

---
+++ Project/StudentAPI/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentAPI
+{
+    public class Student
+    {
+
+        private String name;
+        private int age;
+        private int id;
+        public String Name {  get; set; }
+
+        public int Id {  get; set; }
+
+        public float Fees {  get; set; }
+    }
+}
