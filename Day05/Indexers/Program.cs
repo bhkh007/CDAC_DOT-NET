e31c70d1c79a5ed7e78534b35cf9ef38b2439578
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-30 00:14:41 +0530

    Completed classwork indexers

---
+++ Day05/Indexers/Program.cs
@@ -0,0 +1,54 @@
+﻿
+﻿using System.Collections;
+
+namespace Indexers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //ArrayList o1 = new ArrayList();
+            //o1[0] = 100;
+            ////o1.this[0] = 100;
+            //Console.WriteLine(o1[0]);
+
+
+            Class1 o = new Class1();
+            //o1.Prop1 = 100;  //set
+            o[0] = 100;  //set
+            o[1] = 200;
+            o[2] = 300;
+            //          Console.WriteLine(o.Prop1);  //get
+            Console.WriteLine(o[0]); //get
+            Console.WriteLine(o[1]);
+            Console.WriteLine(o[2]);
+        }
+    }
+    public class Class1
+    {
+        private int prop1;
+        public int Prop1
+        {
+            set
+            {
+                prop1 = value;
+            }
+            get
+            {
+                return prop1;
+            }
+        }
+        int[] arr = new int[3];
+        public int this[int index]
+        {
+            set
+            {
+                arr[index] = value;
+            }
+            get
+            {
+                return arr[index];
+            }
+        }
+    }
+}
