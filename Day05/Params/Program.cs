f04ebd5542ad87e47154c45190a43a98e9506b5c
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-29 19:04:48 +0530

    Day 05 classwork params

---
+++ Day05/Params/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ParamsExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Add(1, 2, 3));
+            Console.WriteLine(Add(1, 2, 3, 4, 5));
+            Console.WriteLine(Add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        }
+        //       static int Add(params int[] arr)
+        //       static int Add(params int[] arr, int a) //error - params needs to be last
+        static int Add(int a, params int[] arr)
+        {
+            int sum = 0;
+            foreach (int item in arr)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
