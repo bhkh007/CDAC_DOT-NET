6c4f2d7b962b31e3c4d94b825dfd174b7d0f0353
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-03 10:50:29 +0530

    completed Task 2 classwork

---
+++ Day08/Tasks2/Program.cs
@@ -0,0 +1,76 @@
+﻿//calling a method with void return type using Task.Run and Task.Factory.StartNew
+namespace Example2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Action objAction1 = Func1;
+            //Task t1 = Task.Run(objAction1); 
+            Task t1 = Task.Run(Func1);
+
+            //Action objAction2 = Func2;
+            //Task t2 = Task.Factory.StartNew(objAction2);
+            Task t2 = Task.Factory.StartNew(Func2);
+
+            Console.ReadLine();
+        }
+        static void Func1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("first Func called from {0},{1}", Thread.CurrentThread.ManagedThreadId, i);
+            }
+        }
+        static void Func2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("second Func called from {0},{1}", Thread.CurrentThread.ManagedThreadId, i);
+            }
+        }
+    }
+}
+
+
+
+//calling a method with void return type and parameters 
+namespace Example3
+{
+    class Program
+    {
+        static void Main3()
+        {
+            //Task t1 = new Task(new Action<object>(Func1), "passed value");
+            Task t1 = new Task(Func1, "passed value");
+            t1.Start();
+
+            Task t2 = Task.Factory.StartNew(Func2, "data");
+
+            // Task t3 = Task.Run()
+            //Task.Run - cannot be used with parameters. 
+            //use anonymous methods instead to access variables declared in calling codestring s = "ccc";
+            string s = "ccc";
+
+            Task.Run(delegate () { Console.WriteLine(s); });
+            Task.Run(() => { Console.WriteLine(s); });
+
+
+            Console.ReadLine();
+        }
+        static void Func1(object obj)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("first Func called {0}{1}", i, obj.ToString());
+            }
+        }
+        static void Func2(object obj)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("second Func called {0},{1}", i, obj.ToString());
+            }
+        }
+    }
+}
