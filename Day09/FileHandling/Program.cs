e97cbfd0078b4a1afc77ae23dacf52f3cca89b9c
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-04 09:08:31 +0530

    Classwork for file handing and working on files

---
+++ Day09/FileHandling/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FileHandling
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            WriteToFormattedTextFile();
+            ReadFromFormattedTextFile();
+        }
+
+        private static void WriteToFormattedTextFile()
+        {
+            StreamWriter writer = File.CreateText("C:\\aaaa\\a.txt");
+
+            writer.WriteLine("Hello World");
+            writer.WriteLine("Line 2");
+            writer.WriteLine("Done for the day");
+
+            writer.Close();
+        }
+        private static void ReadFromFormattedTextFile()
+        {
+            string s;
+            StreamReader reader = File.OpenText("C:\\aaaa\\a.txt");
+            while ((s = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+            reader.Close();
+        }
+    }
+}
---
+++ Day09/FileWorking/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace WorkingWithFiles
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            WriteTextFile();
+            ReadTextFile();
+        }
+
+        private static void WriteTextFile()
+        {
+            //string s = "Hello World";
+
+            //byte[] arr = Encoding.Default.GetBytes(s);
+            ////FileStream stream = File.Open("C:\\aaaa\\a.txt", FileMode.Create);
+            //FileStream stream = File.Open(@"C:\aaaa\a.txt", FileMode.Create);
+            //stream.Write(arr, 0, arr.Length);
+
+            ////stream.Length
+            ////stream.Read()
+
+            //stream.Close();
+            //-------------------------------
+
+
+
+
+            StreamWriter writer = File.CreateText("C:\\aaaa\\a.txt");
+            writer.WriteLine("Hello World");
+            writer.WriteLine("Line 2");
+            writer.WriteLine("230 pm onwards....");
+
+            writer.Close();
+
+
+
+
+        }
+
+        private static void ReadTextFile()
+        {
+            string s;
+            StreamReader reader = File.OpenText("C:\\aaaa\\a.txt");
+            while ((s = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+            reader.Close();
+        }
+
+    }
+}
