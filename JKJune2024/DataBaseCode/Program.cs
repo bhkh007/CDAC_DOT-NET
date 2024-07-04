e2a58e38c9c705396f33c6278e9b96ad597b1035
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-04 09:09:47 +0530

    Method to create Database through C# injecting MSDOTNET Sql Server

---
+++ JKJune2024/DataBaseCode/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Collections.Concurrent;
+
+namespace DataBaseCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Connect();
+        }
+
+        static void Connect()
+        {
+            SqlConnection sqlConn = new SqlConnection();
+
+            try
+            {
+                sqlConn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=JKJune2024;Integrated Security=True";
+                sqlConn.Open();
+                
+                Console.WriteLine("Success");
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+            sqlConn.Close(); 
+            }
+        }
+    }
+}
