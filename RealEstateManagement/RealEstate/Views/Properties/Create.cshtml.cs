219c003f2fc9527f18c8b4bb9e495923136a06de
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-06 00:17:38 +0530

    Updated project created more properties

---
+++ RealEstateManagement/RealEstate/Views/Properties/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstate.Views.Properties
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RealEstateManagement/RealEstate/Views/Properties/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstate.Views.Properties
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
