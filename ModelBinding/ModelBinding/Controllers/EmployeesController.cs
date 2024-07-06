b5d285ef801c86090aecdf60a93f57d85af9df2c
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-06 15:39:46 +0530

    started creating Employee CRUD project featuring Model Binding

---
+++ ModelBinding/ModelBinding/Controllers/EmployeesController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBinding.Models;
+
+namespace ModelBinding.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: EmployeesController
+        public ActionResult Index()
+        {
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee { EmpNo=10,EmpName="Ashwin",Basic=99999,DeptNo=10});
+            empList.Add(new Employee { EmpNo=11,EmpName="Bhavesh",Basic=91999,DeptNo=11});
+            empList.Add(new Employee { EmpNo=12,EmpName="Ayesha",Basic=99164,DeptNo=12});
+            empList.Add(new Employee { EmpNo=13,EmpName="Ashish",Basic=95555,DeptNo=11});
+            empList.Add(new Employee { EmpNo=14,EmpName="Chinmay",Basic=77999,DeptNo=10});
+            return View(empList);
+        }
+
+        // GET: EmployeesController/Details/5
+        public ActionResult Details(int id = 1)
+        {
+            Employee empObj = new Employee();
+            empObj.EmpNo = id;
+            empObj.EmpName = "Ashwin";
+            empObj.Basic = 999999;
+            empObj.DeptNo = 11;
+
+            return View(empObj);
+        }
+
+        // GET: EmployeesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection , int EmpNo,string Name,decimal Basic,int DeptNo)
+        {
+            try
+            {
+
+                string name = collection["Name"];
+                string empno = collection["EmpNo"];
+                string basic = collection["Basic"];
+                string empNo = collection["DeptNo"];
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
