56692a452437b69fa9cb83b3341ec3c0d5d49519
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-08 18:39:28 +0530

    classwork for day 13

---
+++ Day13/DbFirst/Program.cs
@@ -0,0 +1,39 @@
+using DbFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirst
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<Jkjune2024Context>(options =>
+                        options.UseSqlServer(builder.Configuration.GetConnectionString("Jkjune2024Context")));
+
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
