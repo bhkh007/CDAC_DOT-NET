340e2858f682428e2f7d89bc2e6ce984f61f173c
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-08 18:41:54 +0530

    another models

---
+++ MiniProiect/MotionMint/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace MotionMint.Models
+{
+    public class Role
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string SuperAdmin = "SuperAdmin";
+    }
+}
