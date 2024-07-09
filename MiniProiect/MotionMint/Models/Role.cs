d3c56dadf435ff5bd105bf747ab67d37aa25f16f
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-09 23:05:17 +0530

    deleted this project

--- MiniProiect/MotionMint/Models/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MotionMint.Models
-{
-    public class Role
-    {
-        public const string Admin = "Admin";
-        public const string User = "User";
-        public const string SuperAdmin = "SuperAdmin";
-    }
-}
