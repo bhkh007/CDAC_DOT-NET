88aba920d8909d1aff9c5388d56164062fd66884
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-11 12:07:38 +0530

    database table for Booking part

---
+++ dbo.Table_1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].Booking
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] INT NULL, 
+    [User] VARCHAR(50) NULL, 
+    [StartDate] DATE NULL, 
+    [EndDate] DATE NULL, 
+    [TotalPrice] INT NULL, 
+    [Car] NCHAR(10) NULL, 
+    [CarId] INT NULL
+)
