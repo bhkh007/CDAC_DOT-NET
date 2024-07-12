6773de9e1de340998e5584b0831fe108693b9db0
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-12 14:12:52 +0530

    Customer model

---
+++ Mini Project/MotionMint/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Motion_Mint.Models;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    public string? Custname { get; set; }
+
+    public string? Address { get; set; }
+
+    public int? Mobile { get; set; }
+}
