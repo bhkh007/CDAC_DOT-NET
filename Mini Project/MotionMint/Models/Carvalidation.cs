247695733a2608168084e799658a6f8daa437527
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-12 14:11:48 +0530

    updated car validation

---
+++ Mini Project/MotionMint/Models/Carvalidation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Motion_Mint.Models
+{
+    [MetadataType(typeof(CarregMetaData))]
+    public partial class Carreg
+    {
+        public class CarregMetaData
+        {
+            [DisplayName("CarNo")]
+
+            public string? Carno { get; set; }
+
+            [DisplayName("Make")]
+            public string? Make { get; set; }
+
+            [DisplayName("Model")]
+            public string? Model { get; set; }
+
+            [DisplayName("Available")]
+            public string? Available { get; set; }
+        }
+    }
+}
