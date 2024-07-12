f784a9d26f2fd842a554ea6e8931d3073e67eb1d
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-12 14:12:41 +0530

    Car registration model

---
+++ Mini Project/MotionMint/Models/Carreg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Motion_Mint.Models;
+
+public partial class Carreg
+{
+    public int Id { get; set; }
+
+    public string? Carno { get; set; }
+
+    public string? Make { get; set; }
+
+    public string? Model { get; set; }
+
+    public string? Available { get; set; }
+}
