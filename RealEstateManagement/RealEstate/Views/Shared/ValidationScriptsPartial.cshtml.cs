a35dfb6f412f3caa2f0d78bcef67ee428dd2b552
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-13 12:52:00 +0530

    Validation script

---
+++ RealEstateManagement/RealEstate/Views/Shared/ValidationScriptsPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstate.Views.Shared
+{
+    public class ValidationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
