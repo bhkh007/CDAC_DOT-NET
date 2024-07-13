6313e929e87d676ef26c7869c2c7aad8eef0e098
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-13 12:52:23 +0530

    login

---
+++ RealEstateManagement/RealEstate/Views/Shared/LoginPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstate.Views.Shared
+{
+    public class LoginPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
