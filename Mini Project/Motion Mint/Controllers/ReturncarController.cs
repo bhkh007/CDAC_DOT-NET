94cb59402076284466c57adf583034c2e24fcbd9
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-15 08:37:30 +0530

    Created ReturnCarController

---
+++ Mini Project/Motion Mint/Controllers/ReturncarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Motion_Mint.Controllers
+{
+    public class ReturncarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
