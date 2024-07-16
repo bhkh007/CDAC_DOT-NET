9af8651f716409faec4ed68995d60fc342862dda
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-16 15:07:24 +0530

    Rental module

---
+++ Mini Project/Motion Mint/Models/Rental.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Motion_Mint.Models;
+
+public partial class Rental
+{
+    public int Id { get; set; }
+
+    public string? Carid { get; set; }
+
+    public int? Custid { get; set; }
+
+    public int? Fee { get; set; }
+
+    public DateOnly? Sdate { get; set; }
+
+    public DateOnly? Edate { get; set; }
+}
