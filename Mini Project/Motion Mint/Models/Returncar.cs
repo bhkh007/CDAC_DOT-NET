858fb2a30a50748655a1c0c72e7166f04b0793aa
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-16 15:08:12 +0530

    Return car model

---
+++ Mini Project/Motion Mint/Models/Returncar.cs
@@ -0,0 +1,18 @@
+﻿namespace Motion_Mint.Models
+{
+    public partial class Returncar
+    {
+        public int Id { get; set; }
+        public string ? Carno { get; set; }
+
+        public int ? Custid { get; set; }
+       
+        public DateOnly ? Date { get; set; }
+
+        public int ? Elsp { get; set; }
+
+        public int? Fine { get; set; }
+
+
+    }
+}
