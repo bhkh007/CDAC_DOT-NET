c9de5c8f078f9b57dccd5e788b8321c78600dae2
bhkh007 <khandarbhavesh007@gmail.com>
2024-07-16 15:06:36 +0530

    Customer validation model

---
+++ Mini Project/Motion Mint/Models/Customervalidation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Motion_Mint.Models
+{
+    [MetadataType(typeof(CustomerMetaData))]
+    public partial class Customer
+    {
+
+        public class CustomerMetaData 
+        { 
+
+        [DisplayName("CustomerName")]
+        public string? Custname { get; set; }
+
+        [DisplayName("Address")]
+        public string? Address { get; set; }
+
+        [DisplayName("Mobile")]
+        public int? Mobile { get; set; }
+
+
+        }
+    }
+}
